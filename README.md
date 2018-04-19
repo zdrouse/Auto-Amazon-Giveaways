6c2982e4aa31d4fe50c9de1be68ca0411fa39564
rousez <zdrouse@gmail.com>
2018-04-18 22:49:57 -0400

    Removed old code that used selenium, re-starting code design to use pyppeteer

--- README.md
+++ README.md
@@ -1,29 +1,23 @@
 # Auto-Amazon-Giveaways
+[UPDATE] Currently re-designing this project to use pyppeteer instead of selenium.
+This requires Python 3 now.  Giveaway entries are not working yet. 04/18.
+
 Implementation for this project is currently a work-in-progress.  Issues are expected.
 
 ## Install
-[Python 2.7.X](https://www.python.org/downloads/) must be installed.
+[Python 3.6.X](https://www.python.org/downloads/) must be installed.
 
 After Python is installed, open `command-prompt` and use `pip` to install dependencies:
 
- - `pip install selenium`
- - `pip install colorama`
- - `pip install playsound`
-
-ChromeDriver must be installed.
-
- - [Download latest release of ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/downloads) from Google.
- - Place the ChromeDriver in a directory that matches the path you want for the script.
+ - `pip install pyppeteer`
 
 Assuming `git` is installed on the local machine:
 
  - Open `command-prompt` and `cd` to a location/folder to save the project.
  - Perform `git clone https://github.com/zdrouse/Auto-Amazon-Giveaways`.
- - Edit this line for your own chromedriver path:  `chromedriver = webdriver.Chrome('/PATH/TO/chromedriver', chrome_options=opts)`
- 	- The script needs to know where your chromedriver is installed.
 
 ## Run
- - `python give_it_away_now.py`
+ - `python giveaway.py`
 
 ## Profit
 ![bezos](http://i.imgur.com/L8yRHGN.jpg)
--- lib/giveaway.py
+++ lib/giveaway.py
@@ -1,6 +1,5 @@
 import asyncio
 from pyppeteer import launch
-import time
 import getpass
 import logging
 
@@ -10,22 +9,26 @@ class GiveAwayBot(object):
 		self.password = None
 		self.browser = None
 
-	async def _nav_to_ga(self):
-		self.browser= await launch()
-		page = await self.browser.newPage()
-		await page.goto('https://www.amazon.com/ga/giveaways')
-		return page
+	async def _nav_to_ga(self, login_page):
+		ga_page = await login_page.goto('https://www.amazon.com/ga/giveaways')
+		return ga_page
 
-	async def _login(self, init=True):
+	async def login(self, init=True):
+		email_input_box = '#ap_email'
+		password_input_box = '#ap_password'
+		remember_me = '#rememberMe'
+		sign_in_button = '#signInSubmit'
 
 		async def get_browser():
 			return await launch(headless=False)
 
-		def check_for_continue():
-			if self.chromedriver.find_element_by_id('continue'):
-				return True
+		async def check_for_continue(login_page):
+			continue_button = '#continue'
+			is_continue_present = await login_page.querySelector(continue_button)
+			if is_continue_present:
+				await login_page.click(continue_button)
 			else:
-				return False
+				pass
 
 		print('Logging into Amazon...')
 		if init:
@@ -33,31 +36,32 @@ class GiveAwayBot(object):
 			self.password = getpass.getpass('Enter your Amazon password: ')
 		self.browser = await get_browser()
 		login_page = await self.browser.newPage()
+		await login_page.setViewport({'width': 1800, 'height': 1000})
 		await login_page.goto('https://www.amazon.com/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fgiveaway%2Fhome%2Fref%3Dnav_custrec_signin&switch_account=')
-		email_input_box = '#ap_email'
 		await login_page.type(email_input_box, self.email)
-		continue_button = '#continue'
-		await login_page.click(continue_button)
-		password_input_box = '#ap_password'
-		remember_me = '#rememberMe'
-		sign_in_button = '#signInSubmit'
-		await login_page.waitForSelector(password_input_box, timeout=30000)
+		await check_for_continue(login_page)
+		await login_page.waitForSelector(password_input_box, timeout=5000)
 		await login_page.type(password_input_box, self.password)
-		#await login_page.click(remember_me)
+		#await self.browser.click(remember_me)
 		await login_page.click(sign_in_button)
 		print(email_input_box)
-		print(continue_button)
-		await asyncio.sleep(10)
+		await asyncio.sleep(2)
+		# this will navigate to root Giveaway page after successful login and return the page.
+		await self._nav_to_ga(login_page)
+		await asyncio.sleep(15)
 		#await self.browser.close()
-		#self._nav_to_ga()
 
-	def no_req_giveaways(self):
-		no_req_label = self.chromedriver.find_element_by_class_name('giveawayParticipationInfoContainer')
-		print(no_req_label)
+	async def no_req_giveaways(self, ga_page):
+		no_req_label = '#giveawayParticipationInfoContainer'
+		is_label = await ga_page.querySelector(no_req_label)
+		if is_label:
+			print(is_label + ' found')
+		else:
+			print(is_label)
 
 async def main():
 	ga_bot = GiveAwayBot()
-	await ga_bot._login()
-	#ga_bot.no_req_giveaways()
+	ga_page = await ga_bot.login()
+	await ga_bot.no_req_giveaways(ga_page)
 
 asyncio.get_event_loop().run_until_complete(main())
